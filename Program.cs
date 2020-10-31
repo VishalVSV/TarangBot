09f7c5c48150281287c4c838536eb49c547c81c5
Vishal <vishalsvetrivel@gmail.com>
2020-10-31 14:22:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TarangBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
