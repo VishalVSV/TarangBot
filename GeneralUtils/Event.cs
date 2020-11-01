8723f241033e019a2113dd3608d7e8f48769c855
Shan10Code <jojogithub@gmail.com>
2020-11-01 22:14:33 +0530

    Implemented Almost all(?) meta data, left out Disc APi and server stuff to figure out with vert
    Made Inherited Event Template class

--- GeneralUtils/Event.cs
+++ GeneralUtils/Event.cs
@@ -7,28 +7,22 @@ namespace TarangBot.GeneralUtils
     /*
     Derive Via event Name 
 
-
     Meta Data:
     ---------------------
-    
     Small Help Text -> req
     Descriptive Help Text(Rules)-> req
     
     Disc API Stuff - Discuss with Vert when back:
     -------------------------------
-    Channels => Set up files (Load From JSON and attach to class)
+    Voice Channels => Set up files (Load From JSON and attach to class)
+        => Make this ulong list
     
     Event Attr:
     -------------------------------
     Max participants per school -> req
     isTeamEvent 
     isFlagged 
-    EventDates DateTime[], Make Tuple of MaxSize 2
-
-
-    Choices:
-    CamelCase or Underscore(ew)
-    Properties?
+    EventDates DateTime[]
 
      */
 
@@ -36,17 +30,37 @@ namespace TarangBot.GeneralUtils
     {
         //Help text
         public abstract string SmallHelpText { get; }
-        public abstract string DescriptiveHelpText { get;  } //Rules
+        public abstract string DescriptiveHelpText { get; } //Rules
 
+        //Disc.NET Attrs
+        //Get Back to this when Server Structure is figured out
+        //public abstract ulong[] channels { get; }
+
+        //Event Attrs
+
+        //Optionals
         public virtual bool isFlagged { get; } = false;
         public virtual bool isTeam { get; } = false;
 
+        //Must Set
+        public abstract int MaxParticipants { get; }
+        public abstract DateTime[] EventDates { get; }
+
     }
 
-    public class BnT : Event
+    public class Template : Event
     {
         public override string SmallHelpText { get; } = "Test";
         public override string DescriptiveHelpText { get; } = "Big Text";
+
         public override bool isFlagged { get; } = true;
+        public override int MaxParticipants { get; } = 2;
+
+        public override DateTime[] EventDates { get; } = new DateTime[]{
+            new DateTime(2020,12,4),
+            new DateTime(2020,12,5)
+        };
+
+        //public override ulong[] channels => throw new NotImplementedException();
     }
 }
